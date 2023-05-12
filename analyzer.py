af7057e0b24659b41a498e6fd545eb5d119abf01
toribraun <torribraun161@gmail.com>
2023-05-12 23:55:08 +0500

    some changes

--- analyzer.py
+++ analyzer.py
@@ -2,7 +2,6 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqUtils import gc_fraction
 from collections import Counter
-import argparse
 import matplotlib.pyplot as plt
 from Bio.Blast import NCBIWWW
 from Bio import SearchIO
@@ -49,15 +48,15 @@ def transcribe(dna_sequence):
 
 
 def translate(rna_sequence):
-    return rna_sequence.translate()
+    return pad_seq(rna_sequence).translate()
 
 
 # чтобы не было ворнинга, что число нуклеотидов в последовательности
 # не кратно трём
-# def pad_seq(sequence):
-#     """ Pad sequence to multiple of 3 with N """
-#     r = len(sequence) % 3
-#     return sequence if r == 0 else sequence + Seq('N' * (3 - r))
+def pad_seq(sequence):
+    """ Pad sequence to multiple of 3 with N """
+    r = len(sequence) % 3
+    return sequence if r == 0 else sequence + Seq('N' * (3 - r))
 
 
 def get_codons(sequence):
@@ -107,52 +106,28 @@ def draw_most_common_amino_acids_plot(sequence):
 
 
 def get_blast_info(file_name):
-    # pdb|8G6R|A Chain A, nsp12 [Porcine epidemic diarrhea virus]
     seq_protein = translate(transcribe(parse_seq_fasta(file_name)))
     result_handle = NCBIWWW.qblast("blastp", "pdb", seq_protein)
     records = SearchIO.read(result_handle, 'blast-xml')
     polymerases = []
-    helicases = []
+    endoribonucleases = []
     for record in records:
         desc = record.description
         if 'polymerase' in desc:
-            polymerases.append(record.id)
-        elif 'helicase' in desc or 'Helicase' in desc:
-            helicases.append(record.id)
+            polymerases.append(record)
+        elif 'Uridylate-specific endoribonuclease' in desc:
+            endoribonucleases.append(record)
         else:
             print(record.id, desc)
 
-    print('polymerases count', len(polymerases))
-    print('helicases count:', len(helicases))
+    print('proteins count:', len(records))
+    print('polymerases count:', len(polymerases))
+    print('Uridylate-specific endoribonucleases count:', len(endoribonucleases))
 
 
-def main():
-    argparser = argparse.ArgumentParser()
-    proc_arg_group = argparser.add_mutually_exclusive_group()
-    proc_arg_group.add_argument('-trb', '--transcribe', nargs=1, type=str,
-                                metavar='sequence_file', dest='transcribe',
-                                required=False, help='Транскрипция '
-                                                     'последовательности из '
-                                                     'файла')
-    a_group = argparser.add_mutually_exclusive_group()
-    a_group.add_argument('sequence_file', type=str, action='store', nargs='?',
-                         help='Путь до файла с геномными данными '
-                              'в формате .fasta')
-    args = argparser.parse_args()
+get_blast_info('covid_sequence.fasta')
 
-    if args.transcribe:
-        print(transcribe(parse_seq_fasta(args.transcribe[0])))
-    elif args.sequence_file:
-        print('Здесь будет полный анализ')
-    else:
-        print('Такой команды нет\n'
-              'Справка -h | --help')
-
-
-# if __name__ == '__main__':
-#     main()
-
-draw_nucleotides_frequency_visualisation(
-    parse_seq_fasta('covid_sequence.fasta'))
-draw_most_common_amino_acids_plot(parse_seq_fasta('covid_sequence.fasta'))
-draw_most_common_codons_frequency(parse_seq_fasta('covid_sequence.fasta'))
+# draw_nucleotides_frequency_visualisation(
+#     parse_seq_fasta('covid_sequence.fasta'))
+# draw_most_common_amino_acids_plot(parse_seq_fasta('covid_sequence.fasta'))
+# draw_most_common_codons_frequency(parse_seq_fasta('covid_sequence.fasta'))
